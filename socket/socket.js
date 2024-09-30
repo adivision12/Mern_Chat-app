87c16bdd114da3aae62142e685de69a986881249
adivision12 <as81750454@gmail.com>
2024-09-30 15:48:36 +0530

    update

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app=express();
 const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:"*",
+        origin:"https://mern-chat-app-4-s043.onrender.com",
         methods:["GET","POST"],
         
     }
