de57551624c3d944276f8a49bda03e11565e291c
adivision12 <as81750454@gmail.com>
2024-09-30 22:18:41 +0530

    update

--- Front-End/src/Home/Right/Input.jsx
+++ Front-End/src/Home/Right/Input.jsx
@@ -78,9 +78,9 @@ export default function Input() {
       
     <form onSubmit={handleSubmit}>
     
-      <div className='flex  justify-center absolute bottom-5 w-[60%] ml-[5%] max-[500px]:w-[90%] '>
+      <div className='flex  justify-center w-[60%] ml-[5%] max-[500px]:w-[90%] '>
        
-        <input type="text" name='message' value={message} onChange={inputHandle} placeholder="Write Something" className="input input-bordered max-[500px]:absolute max-[500px]:focus:bottom-25   bg-slate-900 border-white text-white w-[100%]" />
+        <input type="text" name='message' value={message} onChange={inputHandle} placeholder="Write Something"  className="input input-bordered max-[500px]:focus:bottom-25  bg-slate-900 border-white text-white w-[100%]" />
        <button> <i  className="fa-solid fa-share mx-4 px-3 py-3  text-white border rounded-lg hover:bg-gray-600 text-xl"></i></button>
     </div>
     </form>
