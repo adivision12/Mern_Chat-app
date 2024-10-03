0867c3bef5035732e838dea4fddc19be351bf163
adivision12 <as81750454@gmail.com>
2024-10-03 12:57:49 +0530

    update

--- Front-End/src/Home/Left/Users.jsx
+++ Front-End/src/Home/Left/Users.jsx
@@ -7,11 +7,29 @@ import { useAuth } from '../../Context/AuthProvider.jsx';
 export default function Users() {
   const [authUser,setAuthUser]=useAuth();
   const [allUsers,loading]=AllUsers();
- 
+  const [chatUser,setChatUser]=useState();
+  const { socket } = useSocketContext();
+console.log('user of users')
+  useEffect(()=>{
+    if (socket && socket.connected) {
+      socket.on("rec-message", (newMessage) => {
+        setChatUser(newMessage.receiverId);
+        console.log('chatUser',chatUser)
+      }
+    )}
+  },[chatUser])
   return (<div className='overflow-y-auto h-[82%]'> 
+   {chatUser && allUsers.map((user,idx)=>{
+    if(chatUser===user._id){
+      return <User key={idx} user={user} /> }
+    })}
   {allUsers.map((user,idx)=>{
     if(authUser?._id!==user._id){
-      return <User key={idx} user={user} />
+      if(chatUser && chatUser!=user._id){
+        return <User key={idx} user={user} />
+      }else{
+        return <User key={idx} user={user} />
+      }
     }
   })}
   </div>
