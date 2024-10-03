dd1b2f880161b5168e22e8857ab3d5ca35f3e8f4
adivision12 <as81750454@gmail.com>
2024-10-03 16:13:16 +0530

    update

--- Front-End/src/Home/Right/Message.jsx
+++ Front-End/src/Home/Right/Message.jsx
@@ -18,7 +18,7 @@ export default function ({message}) {
 
     <div>
      { <div className={`chat ${chatName}`}>
-        <div className={`chat-bubble chat-bubble-accent ${chatColor}`}>{message.message}</div>
+        <div className={`chat-bubble chat-bubble-accent mx-2 ${chatColor}`}>{message.message}</div>
         <br />
         <div className='text-sm'> {formatTime}</div>
         </div>
